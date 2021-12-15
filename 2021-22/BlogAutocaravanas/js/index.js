307e267f91e8cf200ac8e185676f7a0d2ee816a3
JMViiUDEZz <jmviudezp2003@gmail.com>
2021-12-15 15:21:19 +0100

    Verificacion del examen

--- 2021-22/BlogAutocaravanas/js/index.js
+++ 2021-22/BlogAutocaravanas/js/index.js
@@ -5,6 +5,7 @@ function inicio(){
     const vmain = document.querySelector('main section');
     const integrales = document.querySelector('header #top nav a#integrales');
     const perfiladas = document.querySelector('header #top nav a#perfiladas');
+    const empresa = document.querySelector('header #top nav a#empresa');
 
     integrales.addEventListener('click', () => {
         console.log('has pinchado en integrales');
@@ -20,6 +21,17 @@ function inicio(){
     perfiladas.addEventListener('click', () => {
         console.log('has pinchado en perfiladas');
         fetch('html/perfiladas.html')
+        .then(respuesta => {
+            return respuesta.text()
+        })
+        .then(respuestaHTML => {
+            console.log(respuestaHTML);
+            vmain.innerHTML = respuestaHTML;
+        })
+    });
+    empresa.addEventListener('click', () => {
+        console.log('has pinchado en empresa');
+        fetch('html/empresa.html')
         .then(respuesta => {
             return respuesta.text()
         })
